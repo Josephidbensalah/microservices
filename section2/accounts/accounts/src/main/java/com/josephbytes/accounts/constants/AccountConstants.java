56c7f2fc7e21122aaba4277be067d2a6f3d77672
youssefidbensalah <youssef.idbensalah@usms.ac.ma>
2025-02-23 23:31:24 +0000

    part 2 api

--- section2/accounts/accounts/src/main/java/com/josephbytes/accounts/constants/AccountConstants.java
+++ section2/accounts/accounts/src/main/java/com/josephbytes/accounts/constants/AccountConstants.java
@@ -9,7 +9,7 @@ public class AccountConstants {
     public static final String ACCOUNT_DELETED = "Account deleted successfully";
     public static final String ACCOUNT_NOT_FOUND = "Account not found";
     public static final String SAVINGS = "Savings";
-    public static final String ADDRESS = "Address";
+    public static final String ADDRESS = "1234 Main St, New York, NY 10001";
     public static final String STATUS_201 = "201";
     public static final String STATUS_200 = "200";
     public static final String STATUS_404 = "404";
