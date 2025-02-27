c16b55ea2629f5d3709323c2bdb4f7f93a1e6420
Josephidbensalah <joseph.idbensalah@gmail.com>
2025-02-27 18:02:24 +0000

    adding exception handler

--- section2/accounts/accounts/src/main/java/com/josephbytes/accounts/controller/AccountController.java
+++ section2/accounts/accounts/src/main/java/com/josephbytes/accounts/controller/AccountController.java
@@ -3,16 +3,22 @@ package com.josephbytes.accounts.controller;
 import com.josephbytes.accounts.constants.AccountConstants;
 import com.josephbytes.accounts.dto.CustomerDto;
 import com.josephbytes.accounts.dto.ResponseDto;
+import com.josephbytes.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountController {
+
+    private IAccountService iAccountService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountService.createAccount(customerDto);
         return ResponseEntity
                 .status(201)
                 .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
