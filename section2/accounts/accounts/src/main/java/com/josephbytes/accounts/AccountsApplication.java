dd5beb6051a2aae4668b22e8d615f990531274c5
youssefidbensalah <youssef.idbensalah@usms.ac.ma>
2025-03-02 23:25:31 +0000

    completibng accounts Api and starting Docs OpenApi

--- section2/accounts/accounts/src/main/java/com/josephbytes/accounts/AccountsApplication.java
+++ section2/accounts/accounts/src/main/java/com/josephbytes/accounts/AccountsApplication.java
@@ -1,9 +1,32 @@
 package com.josephbytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservices REST API Documentation",
+				description = "EasyBank Accounts Microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+							name = "Joseph Saleh",
+							email = "Josephsaleh@joseph.com",
+							url = "https://josephsaleh.com"),
+				license = @License(name = "Apache 2.0",url = "https://licence.josephsaleh.com")
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EasyBank Accounts Microservices REST API Documentation",
+				url = "https://josephsaleh.com"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- section2/accounts/accounts/src/main/resources/schema.sql
+++ section2/accounts/accounts/src/main/resources/schema.sql
@@ -1,21 +1,24 @@
-create table if not exists `customer` (
-    `customer_id` int AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `mobile_number` varchar(255) NOT Null ,
+Drop table if exists customer;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `customer_id` int AUTO_INCREMENT  PRIMARY KEY,
+  `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `mobile_number` varchar(20) NOT NULL,
     `created_at` date NOT NULL,
-    `created_by` varchar(25) NOT NULL,
-    `updated_at` date NOT NULL,
-    `updated_by` varchar(25) NOT NULL
-);
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+    );
 
-create table if not exists `accounts` (
-    `customer_id` int NOT NULL,
-    `account_number` int AUTO_INCREMENT PRIMARY KEY,
-    `account_type` varchar(255) NOT NULL,
-    `branch_address` varchar(255) NOT NULL,
+drop table if exists accounts;
+drop table if exists account;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `customer_id` int NOT NULL,
+  `account_number` int AUTO_INCREMENT  PRIMARY KEY,
+  `account_type` varchar(100) NOT NULL,
+    `branch_address` varchar(200) NOT NULL,
     `created_at` date NOT NULL,
-    `created_by` varchar(25) NOT NULL,
-    `updated_at` date NOT NULL,
-    `updated_by` varchar(25) NOT NULL
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
 );
