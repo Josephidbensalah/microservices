e6f4b8172529ede281edebba1e56824923ed9630
youssefidbensalah <youssef.idbensalah@usms.ac.ma>
2025-03-23 13:48:08 +0000

    pushing section 4 changes

---
+++ section4/loans/loans/src/main/java/com/josephbytes/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.josephbytes.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans Microservices REST API Documentation",
+				description = "EasyBank Loans Microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Joseph Saleh",
+						email = "joseph@saleh.com",
+						url = "https://josephsaleh.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://licence.josephsaleh.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EasyBank Loans Microservices REST API Documentation",
+				url = "https://josephsaleh.com"
+		))
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
